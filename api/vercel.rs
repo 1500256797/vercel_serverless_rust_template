9198a962d180165c931724413ec326d99295603b
ouhuang <1500256797@qq.com>
2024-11-29 00:55:17 +0800

    fix: fix bugs

--- api/vercel.rs
+++ api/vercel.rs
@@ -2,6 +2,7 @@ use axum::{response::IntoResponse, routing::get, Json, Router};
 use serde_json::json;
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::prelude::*;
+use vercel_serverless_template::setup_server;
 
 mod vercel_layer;
 fn init_tracing() {
@@ -18,6 +19,7 @@ async fn get_index_route() -> impl IntoResponse {
 #[tokio::main]
 async fn main() -> Result<(), vercel_runtime::Error> {
     init_tracing();
+    // let app = setup_server();
     let app = Router::new()
         .route("/", get(get_index_route))
         .layer(TraceLayer::new_for_http());
@@ -31,6 +33,5 @@ async fn main() -> Result<(), vercel_runtime::Error> {
         .map_response(vercel_runtime::process_response)
         .layer(vercel_layer::LambdaLayer::default())
         .service(app);
-
     vercel_runtime::run_service(handler).await
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use axum::{routing::get, Router};
 use state::AppState;
+use tower_http::trace::TraceLayer;
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
 pub mod api;
@@ -7,27 +8,27 @@ pub mod routes;
 pub mod state;
 
 pub fn setup_server() -> Router {
-    #[derive(OpenApi)]
-    #[openapi(
-        paths(
-            routes::people::get_people_info_handler,
-            routes::people::create_people_handler,
-        ),
-        components(
-            schemas(
-                routes::people::PeopleReq,
-                routes::people::PeopleResp,
-            ),
-        ),
-        tags(
-            (name = "hello_axum", description = "axum 模版工程、集成pg、redis、swagger、sqlx")
-        )
-    )]
-    struct ApiDoc;
+//     #[derive(OpenApi)]
+//     #[openapi(
+//         paths(
+//             routes::people::get_people_info_handler,
+//             routes::people::create_people_handler,
+//         ),
+//         components(
+//             schemas(
+//                 routes::people::PeopleReq,
+//                 routes::people::PeopleResp,
+//             ),
+//         ),
+//         tags(
+//             (name = "hello_axum", description = "axum 模版工程、集成pg、redis、swagger、sqlx")
+//         )
+//     )]
+//     struct ApiDoc;
 
     return Router::new()
         .route("/", get(|| async { "hello world" }))
-        .merge(SwaggerUi::new("/swagger").url("/api-docs/openapi.json", ApiDoc::openapi()))
         .merge(routes::people::router())
-        .with_state(AppState {});
+        .with_state(AppState {})
+        .layer(TraceLayer::new_for_http());
 }
